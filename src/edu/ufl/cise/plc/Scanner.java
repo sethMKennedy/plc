a00ea1d73fc8fff386ed2aeca45f2fde7a942eb9
sethMKennedy <62225478+sethMKennedy@users.noreply.github.com>
2022-02-02 01:12:57 -0500

    partial scanner class and more of crafting interpreters code

--- src/edu/ufl/cise/plc/Scanner.java
+++ src/edu/ufl/cise/plc/Scanner.java
@@ -31,19 +31,32 @@ public class Scanner {
         return current>= source.length();
     }
 
+    private char advance() {
+        return source.charAt(current++);
+    }
+
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
     private void scanToken() {
         char c = advance();
         switch (c) {
             case '(' : addToken(LEFT_PAREN); break;
-            case '(' : addToken(RIGHT_PAREN); break;
-            case '}' : addToken(LEFT_BRACE); break;
-            case '{' : addToken(RIGHT_BRACE); break;
+            case ')' : addToken(RIGHT_PAREN); break;
+           // case '}' : addToken(LEFT_BRACE); break;
+          //  case '{' : addToken(RIGHT_BRACE); break;
             case ',' : addToken(COMMA); break;
             case '.' : addToken(DOT); break;
             case '-' : addToken(MINUS); break;
             case '+' : addToken(PLUS); break;
             case ';' : addToken(SEMICOLON); break;
             case '*' : addToken(STAR); break;
+            default  : Lexer.error(line, "That character isn't recognized!"); break;
         }
     }
 }
